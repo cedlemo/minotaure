9f7e52df99a78a1f1eff8a06c7a84df9bce1d4eb
cedlemo <cedlemo@gmx.com>
2018-07-29 10:09:35 +0200

    Prepare function to compute cpu usage

--- lib/System_info.ml
+++ lib/System_info.ml
@@ -94,5 +94,20 @@ module Cpu = struct
     in
     fetch_data {main=None; cores = []} lines
 
+  (* let usage cpu =
+      PrevIdle = previdle + previowait
+      Idle = idle + iowait
 
+      PrevNonIdle = prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal
+      NonIdle = user + nice + system + irq + softirq + steal
+
+      PrevTotal = PrevIdle + PrevNonIdle
+      Total = Idle + NonIdle
+
+      # differentiate: actual value minus the previous one
+      totald = Total - PrevTotal
+      idled = Idle - PrevIdle
+
+      CPU_Percentage = (totald - idled)/totald
+  *)
 end
