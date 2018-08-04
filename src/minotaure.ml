10b236013fbb1d97dddcb7fb6363b3f8c6a7c1af
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-04 11:46:22 +0200

    Rewrite the terminal based on the Notty documentation

--- src/minotaure.ml
+++ src/minotaure.ml
@@ -4,14 +4,20 @@ open Lwt
 
 (* ocamlfind ocamlc -o simple_lwt_terminal_resize -package notty.lwt -linkpkg -g common.ml basics_Lwt_Term_simple_terminal_resize.ml*)
 
-
-module LwtTerm = Notty_lwt.Term
+module T = Notty_lwt.Term
 
 let grid xxs = xxs |> List.map I.hcat |> I.vcat
 let counter = ref 0
 
 let outline attr t =
-  let (w, h) = LwtTerm.size t in
+  let (w, h) = T.size t in
+  let chr x = I.uchar attr (Uchar.of_int x) 1 1
+  and hbar  = I.uchar attr (Uchar.of_int 0x2500) (w - 2) 1
+  and vbar  = I.uchar attr (Uchar.of_int 0x2502) 1 (h - 2) in
+  let (a, b, c, d) = (chr 0x256d, chr 0x256e, chr 0x256f, chr 0x2570) in
+  grid [ [a; hbar; b]; [vbar; I.void (w - 2) 1; vbar]; [d; hbar; c] ]
+
+let outline_dim attr (w, h) =
   let chr x = I.uchar attr (Uchar.of_int x) 1 1
   and hbar  = I.uchar attr (Uchar.of_int 0x2500) (w - 2) 1
   and vbar  = I.uchar attr (Uchar.of_int 0x2502) 1 (h - 2) in
@@ -26,25 +32,36 @@ let size_box cols rows =
   let left_margin = (cols - I.width box) / 2 in
   I.pad ~t:top_margin ~l:left_margin box
 
-let timer () = Lwt_unix.sleep 1.0 >|= fun () -> `Timer
-let event term = Lwt_stream.get (Term.events term) >|= function
+let timer = function
+  | None   -> Lwt.wait () |> fst
+  | Some t -> Lwt_unix.sleep t >|= fun _ -> `Timer
+
+let event e = Lwt_stream.get (T.events e) >|= function
   | Some (`Resize _ | #Unescape.event as x) -> x
   | None -> `End
 
-let rec main term (e, t) (x, y as pos) =
-  let img = I.((outline A.(fg lightred ) term) </> (size_box x y)) in
-  LwtTerm.image term img
-  >>= fun () ->
-    LwtTerm.cursor term (Some pos)
-    >>= fun () ->
-      (e<?>t) >>= function
-        | `Key (`Escape, []) | `Key (`ASCII 'C', [`Ctrl]) -> LwtTerm.release term >>= fun () -> Lwt.return_unit
-        | `Resize (cols, rows) -> main term (e,t) (cols, rows)
-        | `Timer -> let () = counter := !counter + 1 in
-            main term (e,timer()) (x, y)
-        | _ ->Lwt.return () >>= fun () -> main term (e,t) pos
+let term_lwt_timed ?delay ~f =
+  let term = T.create () in
+  let rec loop (e, t) dim =
+    (e <?> t) >>= function
+    | `End | `Key (`Escape, []) | `Key (`ASCII 'C', [`Ctrl]) ->
+        Lwt.return_unit
+    | `Resize dim as evt     -> invoke (event term, t) dim evt
+    | #Unescape.event as evt -> invoke (event term, t) dim evt
+    | `Timer as evt          -> invoke (e, timer delay) dim evt
+  and invoke es dim e =
+    match f dim e with
+    | `Continue      -> loop es dim
+    | `Redraw img -> T.image term img >>= fun () -> loop es dim
+    | `Stop          -> Lwt.return_unit in
+  let size = T.size term in
+  loop (event term, timer delay) size
+
+let f (w,h as dim) = function
+    | `Resize _| `Timer ->
+        let message = size_box w h in
+        `Redraw (I.((outline_dim A.(fg lightred ) dim) </> message ))
+    | _ -> `Continue
 
 let () =
-  let term = LwtTerm.create () in
-  let size = LwtTerm.size term in
-  Lwt_main.run @@ main term (event term, timer ()) size
+  Lwt_main.run @@ term_lwt_timed ~delay:1.0 ~f
